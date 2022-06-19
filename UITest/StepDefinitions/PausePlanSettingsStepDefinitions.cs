8e83b53edcc065cc13c7c3e959d173ca932b4096
f.baqban <fateme.baqban@gmail.com>
2022-06-19 11:48:57 +0200

    Add css selectors assertion

--- UITest/StepDefinitions/PausePlanSettingsStepDefinitions.cs
+++ UITest/StepDefinitions/PausePlanSettingsStepDefinitions.cs
@@ -44,22 +44,50 @@ namespace UITest.StepDefinitions
                  //wait
                  Locators.FatBurnerBeginnerDialoBoxLocators.WaitForFatBurnerDialogBox(Hooks.HookInitialization.driver);
 
-                 //check elements
+                 //check dialog box title and description
                  DialogBoxTitle = Locators.FatBurnerBeginnerDialoBoxLocators
                     .FatBurnerDialogBoxTitle(Hooks.HookInitialization.driver).Displayed;
+
+                    //fat burner dialog box title css assertion
+                    Assert.Equal("rgba(240, 117, 128, 1)", Locators.FatBurnerBeginnerDialoBoxLocators
+                        .FatBurnerDialogBoxTitle(Hooks.HookInitialization.driver).GetCssValue("color"));
 
                 DialogBoxDescription = Locators.FatBurnerBeginnerDialoBoxLocators
                     .FatBurnerDialogBoxDescription(Hooks.HookInitialization.driver).Displayed;
 
+                //check pause button
                 PauseButton = Locators.FatBurnerBeginnerDialoBoxLocators
                     .FatBurnerPauseButton(Hooks.HookInitialization.driver).Displayed;
 
+                    //pause button css assertion
+                    Assert.Equal("rgba(85, 85, 85, 1)", Locators.FatBurnerBeginnerDialoBoxLocators
+                        .FatBurnerPauseButton(Hooks.HookInitialization.driver).GetCssValue("color"));
+
+                    Assert.Equal("rgba(0, 0, 0, 0)", Locators.FatBurnerBeginnerDialoBoxLocators
+                        .FatBurnerPauseButton(Hooks.HookInitialization.driver).GetCssValue("background-color"));
+
+                //check end button
                 EndButton = Locators.FatBurnerBeginnerDialoBoxLocators
                     .FatBurnerEndButton(Hooks.HookInitialization.driver).Displayed;
 
+                    //end button css assertion
+                    Assert.Equal("rgba(255, 255, 255, 1)", Locators.FatBurnerBeginnerDialoBoxLocators
+                        .FatBurnerEndButton(Hooks.HookInitialization.driver).GetCssValue("color"));
+
+                    Assert.Equal("rgba(0, 0, 0, 0)", Locators.FatBurnerBeginnerDialoBoxLocators
+                        .FatBurnerEndButton(Hooks.HookInitialization.driver).GetCssValue("background-color"));
+
+                //check save button
                 SaveButton = Locators.FatBurnerBeginnerDialoBoxLocators
                     .FatBurnerSaveButton(Hooks.HookInitialization.driver).Displayed;
-             }
+
+                    //save button css assertion
+                    Assert.Equal("rgba(136, 136, 136, 1)", Locators.FatBurnerBeginnerDialoBoxLocators
+                        .FatBurnerSaveButton(Hooks.HookInitialization.driver).GetCssValue("color"));
+
+                    Assert.Equal("rgba(0, 0, 0, 0)", Locators.FatBurnerBeginnerDialoBoxLocators
+                        .FatBurnerSaveButton(Hooks.HookInitialization.driver).GetCssValue("background-color"));
+            }
              catch (Exception e)
              {
                  Console.WriteLine(e);
